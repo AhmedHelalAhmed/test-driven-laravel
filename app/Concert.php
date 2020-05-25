a60354632a0812e5bc59dbee58aa14ddbe116d78
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-05-25 12:13:43 +0200

    pass unit test

--- app/Concert.php
+++ app/Concert.php
@@ -8,4 +8,14 @@ class Concert extends Model
 {
     protected $guarded=[];
     protected $dates=['date'];
+
+    /**
+     * Computed property to get the date in special format
+     *
+     * @return mixed
+     */
+    public function getFormattedDateAttribute()
+    {
+        return $this->date->format('F j, Y');
+    }
 }
