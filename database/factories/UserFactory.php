1cd1948a9ef4f76ac796c6efd6fc30121da20f63
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-05-26 05:54:57 +0200

    Add state to factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -43,3 +43,9 @@ $factory->define(Concert::class, function (Faker $faker) {
         'additional_information' => 'Some sample additional information.'
     ];
 });
+
+$factory->state(Concert::class,'published',function ($faker){
+    return [
+        'published_at' => Carbon::parse('-1 week'),
+    ];
+});
